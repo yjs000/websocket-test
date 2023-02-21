d28c945e2f31b102d0723aafaea1a6f23a9db0c0
jsyoon <jsyoon@neighbor21.co.kr>
2023-02-21 16:00:51 +0900

    first project

---
+++ src/main/java/com/websocket/test/wstest/WsTestApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.test.wstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/websocket/test/wstest/WsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.test.wstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
